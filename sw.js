6c0da923fc575243baab6344c7c49ad62e5112b6
mastercook777 <xin.gu1987@gmail.com>
2026-09-20 19:15:45 +0800

    Bump PWA cache for road hotfix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wasteland-survivor-v0.9.3-cover-grenades';
+const CACHE='wasteland-survivor-v0.9.3.1-road-hotfix';
 const FILES=['./','./index.html','./style.css','./game.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
