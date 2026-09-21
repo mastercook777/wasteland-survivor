2e5c1f8b8e579aafd11415e22a24a3ec79caaaa9
mastercook777 <xin.gu1987@gmail.com>
2026-09-21 23:50:33 +0800

    Fix preview service worker cache paths

--- preview/sw.js
+++ preview/sw.js
@@ -1,5 +1,5 @@
-const CACHE='wasteland-survivor-art-reforge-preview-v5';
-const FILES=['./','./index.html','./style.css','./game.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='wasteland-survivor-art-reforge-preview-v6';
+const FILES=['./','./index.html','./style.css?v=6','./game.js?v=6','./manifest.webmanifest','../icon-192.png','../icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
