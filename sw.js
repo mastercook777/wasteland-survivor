ef589ff82632c1f462b54c7dbd009712d4c8ccbe
mastercook777 <xin.gu1987@gmail.com>
2026-09-22 00:22:31 +0800

    Keep production service worker out of preview path

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE='wasteland-survivor-v0.14.1-secondary-diversity';
+const CACHE='wasteland-survivor-v0.14.1-secondary-diversity-r2';
 const FILES=['./','./index.html','./style.css','./game.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
+  const url=new URL(e.request.url);
+  if(url.pathname.includes('/wasteland-survivor/preview/'))return;
   e.respondWith(
     fetch(e.request).then(res=>{
       const copy=res.clone();
